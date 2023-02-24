033911e3319a1e8a8d51518f37ad5ae5c998b43a
dapsu <greenrock46@gmail.com>
2023-02-24 20:53:34 +0900

    refactor: 모듈 위치 변경

--- src/common/pipes/positiveInt.pipe.ts
+++ src/common/pipes/positiveInt.pipe.ts
@@ -1,13 +1,8 @@
-import {
-  PipeTransform,
-  Injectable,
-  ArgumentMetadata,
-  HttpException,
-} from '@nestjs/common';
+import { PipeTransform, Injectable, HttpException } from '@nestjs/common';
 
 @Injectable()
 export class PositiveIntPipe implements PipeTransform {
-  transform(value: number, metadata: ArgumentMetadata) {
+  transform(value: number) {
     if (value < 0) {
       throw new HttpException('value shoule be positive int!', 400);
     }
