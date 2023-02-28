3e17cf02feb1d4d82af83b98abe38f3847b4d0bd
dapsu <greenrock46@gmail.com>
2023-02-28 19:28:33 +0900

    feat: add cors option

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,13 @@ import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT;
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
-  const PORT = process.env.PORT;
+  app.enableCors({
+    origin: `http://localhost:${PORT}`,
+    credentials: true,
+  });
   await app.listen(PORT);
 }
 bootstrap();
