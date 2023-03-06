3fc9edef7efe119f25059ccf61278f642e617d5c
dapsu <greenrock46@gmail.com>
2023-03-06 20:56:56 +0900

    feat: add cat repository

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,17 +1,15 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
-import { Cat } from './cats.schema';
 import { CatRequestDto } from './dto/cats.request.dto';
+import { CatsRepository } from './cats.repository';
 
 @Injectable()
 export class CatsService {
-  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+  constructor(private readonly catsRepository: CatsRepository) {}
 
   async signUp(body: CatRequestDto) {
     const { email, name, password } = body;
-    const isCatExist = await this.catModel.exists({ email });
+    const isCatExist = await this.catsRepository.existsByEmail(email);
 
     if (isCatExist) {
       throw new UnauthorizedException('Already exists the cat!');
@@ -19,7 +17,7 @@ export class CatsService {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const cat = await this.catModel.create({
+    const cat = await this.catsRepository.create({
       email,
       name,
       password: hashedPassword,
